f043f672649e5168529874037db3f335e3033e36
Corey <coreyforbes98@gmail.com>
2020-01-28 21:04:27 +0000

    Update setup.sh
    Changed the flags for docker run

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 
 docker build -t yeroc-sebrof/minceraft .
-docker run -h testing-mine --expose=25565 yeroc-sebrof/minceraft
+docker run -h testing-mine -itp 25565:25565 yeroc-sebrof/minceraft
 
 echo Done!
